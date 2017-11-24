0f2c1ad75dbad8243b459888bd1724b7bb068d59
duinodu <472365351duino@gmail.com>
2017-11-24 15:37:46 +0800

    add lsp.sh

--- shell/fetch_dataset/lsp.sh
+++ shell/fetch_dataset/lsp.sh
@@ -3,16 +3,29 @@
 lsp1='http://192.168.73.132/sam.johnson.io/research/lsp_dataset_original.zip'
 lsp2='http://192.168.73.130/sam.johnson.io/research/lspet_dataset.zip'
 
-if [ ! -d ~/data/lsp ];then
-    mkdir -p ~/data/lsp
+if [ ! -d ~/data/lsp/zip ];then
+    mkdir -p ~/data/lsp/zip
 fi
+
 cd ~/data/lsp
+
+if [ ! -d lsp ];then
+    mkdir lsp
+fi
+
+if [ ! -d lspet ];then
+    mkdir lspet
+fi
+
+cd zip
 
 wget $lsp1
 wget $lsp2
 
-for i in `ls *.zip`;do
-    unzip $i
-done
+cd ../lsp
+unzip ../zip/lsp_dataset_original.zip
+
+cd ../lspet
+unzip ../zip/lspet_dataset.zip
 
 echo "LSP is in ~/data/lsp"
