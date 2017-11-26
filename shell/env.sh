7dcbdf9574fa26fbcf4b8d2ba3d42c45341afea7
DuinoDu <472365351duino@gmail.com>
2017-11-26 15:22:18 +0800

    update env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -1,56 +1,54 @@
 ##!/bin/bash
-#
-#sudo apt-get update
-#sudo apt-get install cmake python-dev python-pip
+# PWD=`pwd`
+#sudo apt-get update -y
+#sudo apt-get install cmake python-dev python-pip -y
 #
 ## git
 #sudo git
 #git config --global user.email "472365351duino@gmail.com"
 #git config --global user.name "DuinoDu"
-#
-## tmux
-#sudo apt-get install tmux
-#sudo apt-get install zsh
-#sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#chsh -s /bin/zsh
-#
+
+
 ## vim
-#sudo apt-get install vim-gtk
+#sudo apt-get install vim-gtk -y
+#cd $PWD
+#cp envrc/vimrc ~/.vimrc
 #mkdir -p ~/.vim/bundle
 #cd ~/.vim/bundle
 #git clone https://github.com/gmarik/Vundle.vim.git
-#echo "copy .vimrc from http://git.oschina.net/duino/codes/7pbqlme9ci218o4hrdztj14/raw?blob_name=vimrc"
-#cd ~
-#wget "http://git.oschina.net/duino/codes/7pbqlme9ci218o4hrdztj14/raw?blob_name=vimrc" -O ~/.vimrc
-#vim ~/.vimrc
-#vim -c 'set fileformat=unix'
-#vim -c 'PluginInstall' 
 #cd ~/.vim
 #git clone https://github.com/duinodu/UltiSnips
+#vim -c 'PluginInstall' 
 
-#
+
 ## python-env
 #cd ~
-#pip install --upgrade pip
-#pip install virtualenv
+#sudo pip install --upgrade pip
+#sudo pip install virtualenv
 #virtualenv py2
 #virtualenv -p python3 py3
 #source ~/py2/bin/activate
-
+#
 ## pytorch
-cd ~
-mkdir src && cd src
+#cd ~
+#mkdir src && cd src
 #git clone --recursive https://github.com/pytorch/pytorch
-cd pytorch
-pip install numpy scipy matplotlib pandas
-pip install -r requirements.txt
-python setup.py install
+#cd pytorch
+#pip install numpy scipy matplotlib pandas
+#pip install -r requirements.txt
+#python setup.py install
+#
+## pip package
+#pip install torchvision torchtext opencv-python jupyter
+#pip install sklearn scikit-image
+#pip install tensorflow tensorflow-tensorboard
+#pip install tensorboard-pytorch
+#pip install mxnet
 
-# pip package
-pip install torchvision torchtext opencv-python jupyter
-pip install sklearn scikit-image
-pip install tensorflow tensorflow-tensorboard
-pip install tensorboard-pytorch
-pip install mxnet
+# SLAM
 
-# download dataset
+# tmux
+sudo apt-get install tmux
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
---
+++ shell/install/install_orbslam2.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# OpenCV >= 2.4.6
+sudo apt-get install libopencv-dev -y
+
+# Boost
+sudo apt-get install libboost-dev -y
+
+# OpenGL
+sudo apt-get install -y mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev libglew-dev
+sudo apt-get install -y freeglut3-dev freeglut3
+sudo apt-get install -y build-essential gdb subversion
+sudo apt-get install -y automake autoconf libtool
+sudo apt-get install -y libgtk2.0-dev libxmu-dev libxxf86vm-dev
+sudo apt-get install -y binutils-gold
+sudo apt-get install -y build-essential
+
+# Pangolin
+cd ~/src/orbslam2
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+make build && cd build && cmake .. && makei -j5 && sudo make install
+
+# Eigen3
+cd ~/src/orbslam2
+wget http://bitbucket.org/eigen/eigen/get/3.2.8.tar.gz
+tar zxvf 3.2.8.tar.gz
+cd eigen*
+make build && cd build && cmake .. && make && sudo make install
+
+# BLAS and LAPACK
+sudo apt-get install -y libblas-dev
+sudo apt-get install -y liblapack-dev
+
+# DBoW2
+cd ~/src/orbslam2
+git clone https://github.com/dorian3d/DBoW2.git
+cd DBoW2
+mkdir build && cd build && cmake .. && make && sudo make install
+
+# g2o
+cd ~/src/orbslam2
+git clone https://github.com/RainerKuemmerle/g2o.git
+cd g2o 
+mkdir build && cd build && cmake .. && make && sudo make install
+
+# orb-slam2
+git clone https://github.com/raulmur/ORB_SLAM2.git ORB_SLAM2
+cd ORB_SLAM2
+chmod +x build.sh
+./build.sh
+
+
+# Download dataset
+cd ~/src/orbslam2
+wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_xyz.tgz
+tar -zxvf rgbd_dataset_freiburg1_xyz.tgz
+rm -rf rgbd_dataset_freiburg1_xyz.tgz
+
+# Run a demo
+cd ~/src/orbslam2/ORB_SLAM2/Examples/Monocular
+./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml ../../../rgbd_dataset_freiburg1_xyz
