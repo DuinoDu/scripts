a6e5956df9c447fb425aa1c2a34ef71034818404
duinodu <472365351duino@gmail.com>
2017-12-02 10:34:51 +0800

    update

--- shell/fetch_latex_template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-iccv2015='http://www.pamitc.org/iccv15/files/iccv2015AuthorKit.zip'
-wget $iccv2015
-unzip iccv2015AuthorKit.zip
-mv iccv2015AuthorKit/latex .
-rm iccv2015AuthorKit* -rf
