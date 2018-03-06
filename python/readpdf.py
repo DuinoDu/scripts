54c81bd708d0a7faf67ecb7de2af13818249649b
DuinoDu <472365351duino@gmail.com>
2018-03-06 10:11:35 +0800

    update readpdf.py

--- python/readpdf.py
+++ python/readpdf.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-'''
-################
-Tool Description
-
-Usage:
-python main.py title.pdf
-or
-readpdf title.pdf
-################
-'''
-
+from __future__ import print_function
 import os
+import argparse
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
@@ -54,24 +44,27 @@ def readpdf(pdf_file, out_file='./pdf_out.txt'):
             outfp.flush()
             outfp.close()
         except Exception, e:
-             print "Exception:%s",e
+             print("Exception:%s"%e)
     pdfTotxt(pdf_file, out_file)
    
-def load_abstract(txt_file):
-    start = "abstract"
-    end = "introduction"
+def load_string(txt_file, split=['abstract', 'introduction']):
+    start = split[0] 
+    end = split[1] 
     with open(txt_file, 'r') as fid:
         contents = fid.readlines()  
         abstracts = []
         flag = False
-        for c in contents:
+        for ind, c in enumerate(contents):
             if start in c.lower():
                 flag = True
-            if end in c.lower():
-                flag = False
+                if start == '':
+                    start = 'EOF'
             if flag:
                 abstracts.append(c)
+            if end in c.lower():
+                flag = False
 
+    # replace '-'
     new_abstract = ""
     for line in abstracts:
         line = line.strip('\n')
@@ -95,25 +88,32 @@ def test_spacy1(text_str):
     token = doc[0]
     print(token)
 
-def main(argv):
-    pdf_file = argv[1]
+def main(args):
     txt_file = "pdf_out.txt"
 
     if not os.path.exists(txt_file):
-        print "Parsing ", pdf_file, "..."
-        readpdf(pdf_file, txt_file)
+        print("Parsing " + args.pdf + "...")
+        readpdf(args.pdf, txt_file)
     else:
-        print "{} exists. Read it.".format(txt_file)
+        print("{} exists. Read it.".format(txt_file))
 
-    abstract_str = load_abstract(txt_file)
-    print abstract_str
+    if args.title:
+        title_str = load_string(txt_file, ['', 'abstract'])
+        print('Title:')
+        print(title_str)
+
+    if args.abstract:
+        abstract_str = load_string(txt_file, ['abstract', 'introduction'])
+        print('Abstract:')
+        print(abstract_str)
 
     #test_spacy1(abstract_str)
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print(__doc__)
-    else:
-        main(sys.argv)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parse arxiv paper')
+    parser.add_argument('--pdf', default='', type=str, help='input pdf file')
+    parser.add_argument('--title', default=1, type=int, help='print title')
+    parser.add_argument('--abstract', default=1, type=int, help='print abstract')
+    args = parser.parse_args()
+    main(args)
