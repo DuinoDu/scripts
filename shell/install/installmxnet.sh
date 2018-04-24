fd26de76098fdf8f909d4b7a2f967fd1fe574716
DuinoDu <472365351duino@gmail.com>
2018-04-24 16:29:12 +0800

    update installmxnet

--- shell/install/installmxnet.sh
+++ shell/install/installmxnet.sh
@@ -10,18 +10,26 @@ sudo apt-get install -y libopencv-dev
 sudo apt-get install libc6-dev-i386
 
 cd ~/src
-git clone --recursive https://github.com/apache/incubator-mxnet
+if [ ! -d incubator-mxnet ];then
+    git clone --recursive https://github.com/apache/incubator-mxnet
+else
+    cd incubator-mxnet
+    git pull
+    cd ..
+fi
+
 
 # add ROI Align
 git clone https://github.com/TuSimple/mx-maskrcnn
 cd mx-maskrcnn/rcnn/CXX_OP/
-cp roi_align.cc roi_align.cu roi_align-inl.h incubator-mxnet/src/operator/
-cp *v1* incubator-mxnet/src/operator/contrib
-cd ~/src
-rm mx-maskrcnn -rf
+cp roi_align.cc roi_align.cu roi_align-inl.h ~/src/incubator-mxnet/src/operator/
+cp *v1* ~/src/incubator-mxnet/src/operator/contrib
+cd ~/src && rm mx-maskrcnn -rf
+
 
 cd incubator-mxnet
-make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1
+#make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1
+make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=0 USE_CUDNN=0
 
 cd python
 pip install --upgrade pip
