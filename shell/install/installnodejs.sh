69e32a6af20b8d6d64dab2b02d51735a32252797
duinodu <472365351duino@gmail.com>
2018-06-27 20:52:49 +0800

    update

--- shell/install/installnodejs.sh
+++ shell/install/installnodejs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+version=6.14.3
+
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
 git clone https://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
-nvm install 4.7.0
+nvm install $version 
 nvm_install_latest_npm
 npm config set registry https://registry.npm.taobao.org
 npm config get registry
