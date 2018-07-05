a3325ba52c02fdb8e0b14e1dc0774058e1f963dc
min.du <min.du@yz-gpu007.hogpu.cc>
2018-07-05 10:21:05 +0800

    update oh-my-zsh

--- shell/envrc/oh-my-zsh.sh
+++ shell/envrc/oh-my-zsh.sh
@@ -2,15 +2,30 @@
 
 #sudo apt-get install tmux zsh curl
 
-# install zsh from src
-wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download
-mkdir zsh && tar -xvzf zsh.tar.gz -C zsh --strip-components 1
-cd zsh
-./configure --prefix=$HOME
+# install ncurses
+wget ftp://invisible-island.net/ncurses/ncurses.tar.gz
+tar -zxvf ncurses.tar.gz
+cd ncurses
+export CXXFLAGS=' -fPIC'
+export CFLAGS=' -fPIC'
+./configure --prefix=$HOME/.local --enable-shared
 make
-make install
+# Deduce environment information and build private terminfo tree
+cd progs
+./capconvert
+cd ..
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#chsh -s /bin/zsh
+## install zsh from src
+#wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download
+#mkdir zsh && tar -xvzf zsh.tar.gz -C zsh --strip-components 1
+#cd zsh
+#./configure --prefix=$HOME
+#make
+#make install
+#
+#
+## oh-my-zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#
+##chsh -s /bin/zsh
