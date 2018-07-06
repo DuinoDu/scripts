53986b491e689536adc81e84baf5f2e24ac47f78
min.du <min.du@hobot.cc>
2018-07-06 13:29:45 +0800

    update

--- cmd/nodejs.bat
+++ cmd/nodejs.bat
@@ -3,6 +3,8 @@
 curl https://nodejs.org/dist/v8.11.3/node-v8.11.3-x64.msi --output ~/node-v8.11.3-x64.msi
 msiexec.exe ~/node-v8.11.3-x64.msi
 
+echo "npm install -g windows-build-tools in powershell with admin mode"
+
 gatsby new gatsby-blog https://github.com/gatsbyjs/gatsby-starter-blog
 cd gatsby-blog
 gatsby develop
