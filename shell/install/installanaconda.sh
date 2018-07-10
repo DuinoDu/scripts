e72f68eb8bfb66a1e53f50b7f58596384d426583
duiondu <472365351duino@gmail.com>
2018-07-10 19:49:35 +0800

    update

---
+++ shell/install/installanaconda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+cd ~/
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+chmod +x Anaconda3-5.2.0-Linux-x86_64.sh
+./Anaconda3-5.2.0-Linux-x86_64.sh
