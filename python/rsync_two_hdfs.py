c346f716dd48655b223f8e6d0eebde153727c6f4
duiondu <472365351duino@gmail.com>
2018-09-27 13:52:16 +0800

    update

--- python/rsync_two_hdfs.py
+++ python/rsync_two_hdfs.py
@@ -21,16 +21,31 @@ def main(args):
     if 'not found' in ret:
         raise IOError, "hadoop not found"
 
-    ret1 = run('hadoop fs ls %s' % args.hdfs1).split('\n')
-    for l in ret1:
-        print(l)
-    
+    if not args.hdfs1.endswith('/'):
+        args.hdfs1 += '/'
+    if not args.hdfs2.endswith('/'):
+        args.hdfs2 += '/'
 
+    ret = run('hadoop fs -ls %s' % args.hdfs1).split('\n')
+    files1 = [x.split(' ')[-1].split(args.hdfs1)[-1] for x in ret if 'hdfs' in x]
+
+    ret = run('hadoop fs -ls %s' % args.hdfs2).split('\n')
+    files2 = [x.split(' ')[-1].split(args.hdfs2)[-1] for x in ret if 'hdfs' in x]
+
+    files_to_hdfs2 = [x for x in files1 if x not in files2]
+    for f in files_to_hdfs2:
+        run('hadoop fs -cp %s%s %s' % (args.hdfs1, f, args.hdfs2))
+
+    files_to_hdfs1 = [x for x in files2 if x not in files1]
+    for f in files_to_hdfs1:
+        run('hadoop fs -cp %s%s %s' % (args.hdfs2, f, args.hdfs1))
+    
+    print('finish')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='rsync folders in two hdfs')
-    parser.add_argument('--hdfs1', default='hdfs://hobot-bigdata/user/min.du/common/models', type=str, help='first hdfs path')
-    parser.add_argument('--hdfs2', default='hdfs://hobot-mosdata/user/min.du/common/models', type=str, help='second hdfs path')
+    parser.add_argument('--hdfs1', default='hdfs://hobot-bigdata/user/min.du/common/models/', type=str, help='first hdfs path')
+    parser.add_argument('--hdfs2', default='hdfs://hobot-mosdata/user/min.du/common/models/', type=str, help='second hdfs path')
     args = parser.parse_args()
     main(args)
