7d00426b5288d72880b67dfaf7e2336193759eba
杜敏 <dumin@dumindeMacBook-Pro.local>
2019-02-10 18:19:48 +0800

    update

--- shell/install/install_gatsby.sh
+++ shell/install/install_gatsby.sh
@@ -9,3 +9,9 @@ cd ~/project
 gatsby new gatsby-blog https://github.com/gatsbyjs/gatsby-starter-blog
 cd gatsby-blog
 gatsby develop
+
+# if failed,
+# cd gatsby-blog
+# cnpm install
+# gatsby build
+# gatsby serve
