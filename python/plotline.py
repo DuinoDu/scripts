cd83e358f4e63a50d56b03b06c5f5f48b1fedc7a
min.du <min.du@horizon.ai>
2019-03-15 16:12:01 +0800

    mv jsonloader to tools.hobot

--- python/plotline.py
+++ python/plotline.py
@@ -10,25 +10,23 @@ import sys
 import matplotlib.pyplot as plt
 
 y = [x.strip().split(' ')[0] for x in sys.stdin.readlines()]
-#y = [48.7, 50.0, 51.3, 53.6, 53.6, 54.0, 55.0, 55.1, 55.5]
 y_f = []
 for ind, elem in enumerate(y):
     try:
-        y_f.append(float(elem))
+        y_f.append(float(elem)*100)
     except Exception as e:
         print('Bad value: {}'.format(elem))
 y = y_f
-x = [i for i in range(len(y))]
-#x = [22, 30, 40, 50, 60, 70, 80, 90, 100]
+x = [i*10 for i in range(len(y))]
 
 
-plt.plot(x, y, color='red', label='AP', linewidth=2)
+plt.plot(x, y, color='red', label='lmks2lmk1pose-baseline', linewidth=2)
 
 plt.xlim([x[0], x[-1]])  
-plt.ylim([min(y) - 5, max(y) + 5])  
+plt.ylim([0, max(y)+1])  
 plt.xlabel(u'epoch')  
-plt.ylabel(u'AP')  
-plt.title(u'AP-epoch')  
+plt.ylabel(u'err')  
+plt.title(u'err-epoch')  
 plt.legend(loc="lower right")  
 plt.grid()
 plt.show()  
