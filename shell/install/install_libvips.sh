52bcc08991fe1c9fa32c333815f6cdc335e8e1fd
min.du <min.du@horizon.ai>
2019-05-29 20:27:19 +0800

    add gitst

--- shell/install/install_libvips.sh
+++ shell/install/install_libvips.sh
@@ -5,6 +5,10 @@ mkdir src_libvips && cd src_libvips
 wget https://github.com/libvips/libvips/releases/download/v8.8.0-rc2/vips-8.8.0-rc2.tar.gz
 tar -zxvf vips-v8.8.0-rc2.tar.gz
 
+prefix=`which node`/../../
+
 cd libvips-8.8.0
-./configure
+./configure --prefix=$prefix
 make
+make install
+echo "installed in $prefix "
